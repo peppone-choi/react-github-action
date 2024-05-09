b24f6fdb4e305b2c38a247cff565238ff7efbd93
peppone-choi <peppone.choi@gmail.com>
2024-05-09 18:43:03 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    setCount(count - 1);
+  };
+
+  const toggle = () => {
+    setIsDisabled(!isDisabled);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 data-testid="counter">{count}</h3>
+        <button data-testid="minus-button" disabled={isDisabled} onClick={decrease}>
+          -
+        </button>
+        <button data-testid="plus-button" disabled={isDisabled} onClick={increase}>
+          +
+        </button>
       </header>
+      <button
+        data-testid="on/off-button"
+        style={{
+          backgroundColor: "blue",
+        }}
+        onClick={toggle}
+      >
+        on/off
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,52 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("카운터는 0부터 시작한다", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test("마이너스 버튼은 - 텍스트가 있다", () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId("minus-button");
+  expect(minusButtonElement).toHaveTextContent("-");
+});
+
+test("플러스 버튼은 + 텍스트가 있다", () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toHaveTextContent("+");
+});
+
+test("플러스 버튼을 클릭하면 카운터가 1 증가한다", () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  const counterElement = screen.getByTestId("counter");
+  fireEvent.click(plusButtonElement);
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test("마이너스 버튼을 클릭하면 카운터가 1 감소한다", () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId("minus-button");
+  const counterElement = screen.getByTestId("counter");
+  fireEvent.click(minusButtonElement);
+  expect(counterElement).toHaveTextContent(-1);
+});
+
+test("on and off button은 파란 색상을 가진다.", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("on/off-button");
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+});
+
+test("on and off button을 클릭하면 마이너스 버튼과 플러스 버튼이 비활성화 된다", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("on/off-button");
+  const plusButtonElement = screen.getByTestId("plus-button");
+  const minusButtonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  expect(plusButtonElement).toBeDisabled();
+  expect(minusButtonElement).toBeDisabled();
 });
